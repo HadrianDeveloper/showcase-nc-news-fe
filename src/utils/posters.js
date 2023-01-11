f40bd644858bbfa4d3e20a7388e42e444989661d
HadrianDeveloper <hadrian.developer@outlook.com>
2023-01-11 14:33:57 +0000

    top up

--- src/utils/posters.js
+++ src/utils/posters.js
@@ -4,14 +4,19 @@ const api = axios.create({
     baseURL: 'https://persian-blue-millipede-veil.cyclic.app/api'
 });
 
+
 export function postComment(id, input, username) {
     const post = {
-        author: username,
-        article_id: id,
+        username: username,
         body: input
     };
 
-    return api.post(`/api/articles/${id}/comments`, post)
+    const test = {
+        username: 'grumpy19',
+        body: 'Lupus non timet canem latrantem! Carpe diem!'
+    };
+
+    return api.post(`/api/articles/${id}/comments`, test)
         .then((res) => console.log(res))
         .catch((err) => console.log(err))
 };
